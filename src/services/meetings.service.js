9a8b23bccd56da032edb7d90620d3a2c5d3a7bcc
LeeNakKyu <nakku1055@naver.com>
2024-01-22 11:39:16 +0900

    script: [autoDelete] node-cron 설치

--- src/services/meetings.service.js
+++ src/services/meetings.service.js
@@ -71,10 +71,11 @@ export class MeetingsService {
 
   // 리스트 삭제
   deleteMeeting = async (id) => {
+    // 미팅방 존재 확인
     const meeting = await this.meetingsRepository.findMeetingById(id);
-
     if (!meeting) throw new Error('미팅방이 존재하지 않습니다.');
 
+    // 미팅방 삭제
     await this.meetingsRepository.deleteMeeting(id);
 
     return meeting;
