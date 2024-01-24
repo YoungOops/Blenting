7b5ca700f83c65e1ecbdf873a4798c3eb06224c8
MinJae-Nun <amvs129@naver.com>
2024-01-24 17:39:49 +0900

    fear: auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,18 @@
-import { Router } from "express";
-import { meetingsRouter } from "./meetings.router.js";
-import { messagesRouter } from "./messages.router.js";
+import { Router } from 'express';
+import { meetingsRouter } from './meetings.router.js';
+import { messagesRouter } from './messages.router.js';
+import { authRouter } from './auth.router.js';
 
 const apiRouter = Router();
 
-
 // 블랜팅
 // 채팅룸
-apiRouter.use("/meetings", meetingsRouter);
+apiRouter.use('/meetings', meetingsRouter);
 
 // 메세지
-apiRouter.use("/messages", messagesRouter);
+apiRouter.use('/messages', messagesRouter);
+
+// 로그인
+apiRouter.use('/auth', authRouter);
 
 export { apiRouter };
