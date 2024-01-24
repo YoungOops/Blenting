0951e65f5bf26ea69db1708fdf05dde2fea8ba37
MinJae-Nun <amvs129@naver.com>
2024-01-24 18:02:48 +0900

    feat: auth

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { AuthController } from '../controllers/auth.controller.js';
-import authMiddleware from '../middlewares/auth.middleware.js';
 
 const authRouter = Router();
 
