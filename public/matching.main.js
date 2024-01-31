5021d8bec0e2d934bee066e04ceda0d8bc65f903
LeeNakKyu <nakku1055@naver.com>
2024-01-31 15:42:28 +0900

    feat: [socketRoom] 프론트 메인페이지, css, 클릭 시 함수 실행하는 js 생성

---
+++ public/matching.main.js
@@ -0,0 +1,5 @@
+import { ClickMatchingButton } from "./js/socket/index.js";
+
+// 버튼 클릭 이벤트 리스너 등록
+const matchingButton = document.getElementById('match');
+matchingButton.addEventListener('click', ClickMatchingButton);
